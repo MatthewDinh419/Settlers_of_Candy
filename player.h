e090c8b69a5555d3b4d816aa5d5da7cad1f52a04
Eric Kim <32148534+EricEKim@users.noreply.github.com>
2018-12-08 23:24:50 -0700

    Pointer map

--- player.h
+++ player.h
@@ -9,14 +9,14 @@ public:
     Player(int player_id);
     std::vector<resource> get_current_resources(){return current_resources;}
     void AddResource(resource resource_to_add){current_resources.push_back(resource_to_add);}
-    void AddBuilding(std::pair <int,int> point, Building b){buildings_.insert({point,b});}
+    void AddBuilding(std::pair <int,int> point, Building *b){buildings_.insert({point,b});}
     void RemoveResource(resource resource_to_remove);
     int get_id(){return player_id_;}
-    std::map<std::pair <int,int>, Building> get_buildings(){return buildings_;}
+    std::map<std::pair <int,int>, Building *> get_buildings(){return buildings_;}
 private:
     int player_id_;
     std::vector<resource> current_resources; //Player's current resources
-    std::map<std::pair <int,int>, Building> buildings_; // players buildings
+    std::map<std::pair<int,int>, Building *> buildings_; // players buildings
 };
 
 #endif // PLAYER_H
